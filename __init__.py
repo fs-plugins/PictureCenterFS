ce8507e048701b36aed1dcaeeebb0ceee22cd22d
shadowrider <plugins@fs-plugins.de>
2024-02-12 09:43:54 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 from os import environ as os_environ
 import gettext
 
+version="8.50"
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
 	os_environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
@@ -12,7 +13,6 @@ def localeInit():
 def _(txt):
 	t = gettext.dgettext("PictureCenterFS", txt)
 	if t == txt:
-		#print "[webradioFS] fallback to default translation for", txt
 		t = gettext.gettext(txt)
 	return t
 
